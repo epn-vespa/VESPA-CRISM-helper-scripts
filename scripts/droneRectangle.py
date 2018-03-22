d718b0e49726b6eb113ca3d6ef1b425cfdc7404d
Mikhail Minin <mminin2010@gmail.com>
2018-03-22 15:10:30 +0100

    Update droneRectangle.py
    bug fixes

--- scripts/droneRectangle.py
+++ scripts/droneRectangle.py
@@ -10,16 +10,18 @@ class droneRectangle():
         self.cornersDeg=np.arctan2(*np.split(self.cornersCircle.T,2))[0]*180/np.pi # calculate degrees
         self.getCornerCoords=lambda q: np.stack([np.sin(q*np.pi/180),np.cos(q*np.pi/180)]).T #convert from degrees to coords
         #math definitions end
-        self.CoordConverter=lambda t: (str((-t[3])+(t[1]*((t[2]) / 2.0))/(math.pi*3390.0*math.cos(((t[4])+(t[0]*((t[2]) / 2.0))/    \
-            (math.pi*3390.0 / 180.0))*math.pi/180.0) / 180.0)),str((t[4])+t[0]*((t[2]) / 2.0)/(math.pi*3390.0 / 180.0)))
+        PlanetaryRadius=6371.0#units in km
+        self.CoordConverter=lambda t: (str((-t[3])+(t[1]*((t[2]) / 2.0))/(math.pi*PlanetaryRadius*math.cos(((t[4])+(t[0]*((t[2]) / 2.0))/    \
+            (math.pi*PlanetaryRadius / 180.0))*math.pi/180.0) / 180.0)),str((t[4])+t[0]*((t[2]) / 2.0)/(math.pi*PlanetaryRadius / 180.0)))
         #converter definitions end
     def giveRotFootprint(self, Rotation: float, Diameter: float, c1mid: float, c2mid: float) -> str:
         """rectangle ratio of 3:4 is assumed"""
         cornersDegRot=self.cornersDeg+Rotation # add rotation from Yaw
         coords=self.getCornerCoords(cornersDegRot) 
         coordsClosed=coords.tolist()+[coords.tolist()[0]]
-        coordsMap=[self.CoordConverter([*coordClosed,10,25,35]) for coordClosed in coordsClosed]
+        coordsMap=[self.CoordConverter([*coordClosed,Diameter,c1mid,c2mid]) for coordClosed in coordsClosed]
         coordsList=[' '.join(i) for i in coordsMap]
         coordsStr=' '.join(coordsList)
         footprint= 'Polygon '+coordsStr
         return footprint
+
